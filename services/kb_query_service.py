cd0ca032848b0474c54bc0003a733d8ece851cad
mukundupadhyay6854 <upadhyay.mukund03@gmail.com>
2026-07-14 21:40:57 +0530

    Update check_kb_collection utility

--- services/kb_query_service.py
+++ services/kb_query_service.py
@@ -226,7 +226,6 @@ def ask_question(
         # --------------------------------------------------
         # Save to Cache
         # --------------------------------------------------
-
         cache_answer(
             question=query,
             embedding=queryEmbedding,
